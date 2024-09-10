40acf69efd01c0dee767ba197c1c98c249269cdf
Damian <hello@damianpetrov.com>
2024-09-10 20:31:43 +1000

    feat: implement food list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const food = [
+	"bananas",
+	"bacon",
+	"pizza",
+	"mango",
+	"nachos",
+	"KFC",
+	"sushi",
+	"tinned spaghetti",
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Hello world!</h1>
+
+			<ul>
+				{food.map((individualFoodItem) => {
+					return <li>{individualFoodItem}</li>;
+				})}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
